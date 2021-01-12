eae61cf18d64024a5eff86035e5845772ac5f164
Cassio Ribeiro <cassioribeiropereira@gmail.com>
2021-01-11 22:32:02 -0300

    primeira versao super pokemon.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment, useState, useEffect } from 'react';
+import Carta from './components/carta/carta';
+import axios from 'axios';
+import Pokemon from './classes/pokemon';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import ReactDOM from 'react-dom';
+
+
 
 function App() {
+  const [habilidade, setHabilidade] = useState();
+  const [quantidadeCartas, setQuantidadeCartas] = useState();
+  const [pokemons, setPokemons] = React.useState<Pokemon[]>([]);
+  const [cartasNaMao, setCartasNaMao] = React.useState<Pokemon[]>([]);
+
+
+  const sorteados = () => {
+    for (let i = 0; i < 32; i++) {
+
+      let num = getRandom(1, 500);
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`)
+        .then(response => {
+          let data = response.data;
+          let poke = new Pokemon(
+            num, data.name,
+            data.sprites.other['official-artwork'].front_default,
+            getStat(data, 0), getStat(data, 1),
+            getStat(data, 2), getStat(data, 3),
+            getStat(data, 4), getStat(data, 5)
+          );
+
+          pokemons.push(poke)
+        })
+
+    }
+    console.log(pokemons)
+
+    setTimeout(() => {
+      cartasNaMao.push(pokemons[0], pokemons[1], pokemons[2], pokemons[3], pokemons[4]);
+      console.log(cartasNaMao)
+      renderizaPokemons();
+    }, 1000)
+
+  }
+
+  const getStat = (data: any, index: any) => {
+    return data.stats[index].base_stat
+  }
+
+  function getRandom(min: any, max: any) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  const setaHabilidade = (habilidadeEscolhida: any) => {
+    setTimeout(() => {
+      setHabilidade(habilidadeEscolhida);
+    }, 1000)
+    setTimeout(() => {
+      alert(habilidade)
+    }, 1000)
+
+  }
+
+
+
+  function renderizaPokemons() {
+    const cards =
+
+      <Container className="mw-100">
+        {habilidade}
+        <Row>
+          {
+            cartasNaMao.map(carta =>
+              <Col className="row d-flex justify-content-center">
+                <Card style={{ width: '18rem' }} >
+                  <Card.Img variant="top" src={carta.img} />
+                  <Card.Body>
+                    <Card.Title>{carta.nome}</Card.Title>
+                    <Card.Text>
+                    <h5 onClick={() => setaHabilidade(carta.hp)}>HP: {carta.hp}</h5>
+                    <h5 id='ataque'>ataque:{carta.ataque}</h5>
+                    <h5 id='defesa'>defesa:{carta.defesa}</h5>
+                    <h5 id='ataque especial'>ataque especial:{carta.ataque_especial}</h5>
+                    <h5 id='defesa especia'>defesa especia:{carta.defesa_especial}</h5>
+                    <h5 id='velocidade'>velocidade:{carta.velocidade}</h5>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            )
+          }
+        </Row>
+      </Container >
+
+      ReactDOM.render(cards, document.getElementById('root'));
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Fragment>
+
+      <button onClick={sorteados}>Sortear</button>
+      <button onClick={() => setaHabilidade(50)}></button>
+      <div id='root'>
+        
+
+      </div>
+    </Fragment>
   );
+
 }
+
+
 
 export default App;
---
+++ src/classes/pokemon.tsx
@@ -0,0 +1,14 @@
+
+export default class Pokemon {
+    constructor(
+                public id: any,
+                public nome: any,
+                public img: any,
+                public hp: any,
+                public ataque: any,
+                public defesa: any,
+                public ataque_especial: any,
+                public defesa_especial: any,
+                public velocidade: any
+                ){}
+}
